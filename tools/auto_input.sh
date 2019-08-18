a9132845d57f4416d048e27facd99c0c9c196354
aonori <aonori@aonr.jp>
2019-08-19 05:09:39 +0900

    auto_input.rbのbashスクリプト版を作った、えらい。

---
+++ tools/auto_input.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# - written by @aonrjp
+
+TYPES=("houshin" "soukatsu")
+SECTIONS=("kaikei" "kensui" "soumu" "syogai" "system" "zentai")
+
+for type in ${TYPES[@]}
+do
+    for section in ${SECTIONS[@]}
+    do
+        # 所定ディレクトリ以下に含まれるtexファイルを取得してソート
+        files=`find src/${type}/${section} -type f -name "*.tex" | sort`
+        # input文を生成 (シェルによって\\\\が\\となり、正規表現で\\は\を表す)
+        doc=`echo "${files}" | sed -e 's/^/\\\\input{/' | sed -e 's/$/}/'`
+        # 書き込む
+        echo "${doc}" > src/${type}/${section}.tex
+    done
+done
