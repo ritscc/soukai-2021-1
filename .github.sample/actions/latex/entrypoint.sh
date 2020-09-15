48cecb0a9c63f2372f06d7b2e040e04cb46e2dd0
Aonori <aonori@aonr.jp>
2020-09-15 16:38:22 +0900

    GitHub actionsへのCIツール対応 (#5)
    * [Add] unagi github actions workflow
    
    * [Add] LaTeX build github actions workflow
    
    * [Delete] files no longer needed on GitHub
    
    * [Add] collision emoji on exit code filter

---
+++ .github.sample/actions/latex/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eux
+
+# Run auto input script
+tools/auto_input.sh
+
+# Build document.pdf
+ptex2pdf -interaction=nonstopmode -l -ot -kanji=utf8 -synctex=1 document.tex
+
+#  Release generated PDF (only PR targeat develop or master)
+# -----------------------------------------------------------
+#   https://qiita.com/denkiuo604/items/137a1b3fc1955cfb9c58
+
+if [ $GITHUB_BASE_REF = "develop"] || [ $GITHUB_BASE_REF = "master" ]; then
+  # create release
+  res=`curl -H "Authorization: token $GITHUB_TOKEN" \
+  -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+  -d "
+  {
+    \"tag_name\": \"ver.${GITHUB_SHA:0:7}\",
+    \"target_commitish\": \"$GITHUB_SHA\",
+    \"name\": \"ver.${GITHUB_SHA:0:7}\",
+    \"draft\": false,
+    \"prerelease\": true
+  }"`
+
+  # Extract release id
+  rel_id=`echo ${res} | jq '.id'`
+
+  # Upload built document.pdf
+  curl -H "Authorization: token $GITHUB_TOKEN" \
+    -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=document.pdf \
+    --header 'Content-Type: application/pdf' \
+    --upload-file document.pdf
+fi
